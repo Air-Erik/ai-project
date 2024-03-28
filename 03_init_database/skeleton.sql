30d7bdbef9d02f943f06a57731f240085d0c69a4
Air Erik <airerik@yandex.ru>
2024-03-28 15:11:14 +0300

    Замена функции скелетизации на более продвинутую

--- 03_init_database/skeleton.sql
+++ 03_init_database/skeleton.sql
@@ -1,3 +1,3 @@
 INSERT INTO workflow.skelet_pipe_data (id, skelet)
-SELECT id, ST_CollectionExtract(ST_StraightSkeleton(mask), 2)
+SELECT id, ST_CollectionExtract(ST_ApproximateMedialAxis(mask), 2)
 FROM workflow.raw_mask_data;
