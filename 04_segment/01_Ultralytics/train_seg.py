7254dd7b785b5d910c1d23adc2a6079988ab36e9
Air Erik <airerik@yandex.ru>
2024-04-10 16:25:42 +0300

    Добавлено автоматическое присваивание тегов по гиперпараметров для ClearML

--- 04_segment/01_Ultralytics/train_seg.py
+++ 04_segment/01_Ultralytics/train_seg.py
@@ -3,6 +3,21 @@ from ultralytics import YOLO
 import torch
 
 
+torch.backends.cuda.matmul.allow_tf32 = True
+
+
+# Функция для создания списка тегов из словаря гиперпараметров
+def tags_for_clearml(args):
+    args_list = []
+
+    args_1 = dict(args)
+    del args_1['data']
+
+    for i in args_1:
+        args_list.append(i + '=' + str(args_1[i]))
+    return args_list
+
+
 def main():
     # Проверка работы CUDA и вычисления на GPU
     print(torch.cuda.is_available())
@@ -10,33 +25,37 @@ def main():
     print(torch.cuda.current_device())
     print(torch.cuda.device(0))
     print(torch.cuda.get_device_name(0))
+    print(torch.backends.cuda.matmul.allow_tf32)
 
     # ClearML; Определение модели на которой будет происходить обучение
-    model_name = "yolov8l"
-    dataset_name = 'png_pipe_4cls_long.v1'
+    model_name = "yolov8n"
+    dataset_name = 'png_pipe_6cls_long.v1'
 
-    args = dict(data=f'datasets/{dataset_name}/data.yaml',
-                #optimizer='SGD',
-                epochs=100,
-                imgsz=640,
-                patience=30,
-                weight_decay=0.001,
-                copy_paste=1.0
-                )
+    # Словарь гиперпараметров модели
+    args = dict(
+        data=f'datasets/{dataset_name}/data.yaml',
+        #optimizer='SGD',
+        epochs=100,
+        imgsz=640,
+        patience=30,
+        #weight_decay=0.001,
+        overlap_mask=False,
+        degrees=45,
+        scale=0.1,
+        fliplr=0,
+        mixup=0.5,
+        copy_paste=1
+    )
 
     # ClearML; Создание объекта задачи для clearml, описывает проект и
     # название текущей сессии
     task = Task.init(
         project_name="Segment_png_only",
         task_name=dataset_name,
-        tags=['png',
-              model_name,
-              #f"optimizer={args['optimizer']}",
-              f"epoch={args['epochs']}",
-              f"patience={args['patience']}",
-              f"weight_decay={args['weight_decay']}",
-              f"copy_paste={args['copy_paste']}"
-              ]
+        tags=[
+            model_name,
+            *tags_for_clearml(args)
+            ]
         )
     task.set_parameter("model_variant", model_name)
 
